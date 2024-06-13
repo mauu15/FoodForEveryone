0b8d59576081b9bbc7235f156aa0f0a1821e0fad
Mau <maurizio.mazza@edu.unifi.it>
2024-06-13 14:09:31 +0200

    changed media folder

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,4 +126,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'ffe/media/')
